cd02d5db885a8aa6354e09091f89200c02c40f79
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-03-23 11:09:03 +0300

    commit

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+    // Select the button element
+    const input = document.getElementById('button');
+  
+    // Define the event handler function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Attach event listener
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to activate the event listener
+  addingEventListener();
+  
